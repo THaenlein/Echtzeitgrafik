226840c5084221775af0e2e45c8eec0ee25e484d
THaenlein <tim_24398@gmx.de>
2024-08-02 15:04:40 +0200

    Added model loading examples

---
+++ src/11ModelLoading/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+
+#define GLEW_STATIC
+#include <GL/glew.h> // has to be included first!
+#include <GLFW/glfw3.h>
+#include <assimp/Importer.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include "helper/RootDir.h"
+
+#include "shared/data.h"
+#include "shared/functions.h"
+
+int main(int argc, char** argv)
+{
+    std::cout << "ModelLoading" << std::endl;
+
+    GLFWwindow* window = initAndCreateWindow(true);
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glEnable(GL_DEPTH_TEST);
+
+    GLint shaderProgram = createShaderPipeline(perspectiveVertexShaderSource, perspectiveFragmentShaderSource);
+    GLuint vao, vbo, ebo;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    std::vector<float> vertices;
+    std::vector<unsigned int> indices;
+    loadMeshFromFile("sphere.obj", vertices, indices);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+
+    /* Position attribute */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    /* Color attribute */
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glUseProgram(shaderProgram);
+
+    while (glfwWindowShouldClose(window) == 0)
+    {
+        // clear the window
+        glClearColor(0.0f, 0.1f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Calculate matrices
+        glm::mat4 model = glm::mat4(1.0f), view = glm::mat4(1.0f);
+        glm::mat4 projection;
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(glm::radians(45.0f), float(WIDTH) / float(HEIGHT), 0.1f, 1000.0f);
+
+        // Setting uniforms
+        int modelLoc = glGetUniformLocation(shaderProgram, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        int viewLoc = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        int perspectiveLoc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(perspectiveLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr);
+        glBindVertexArray(0);
+
+        // swap buffer
+        glfwSwapBuffers(window);
+
+        // process user events
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
--- src/shared/functions.h
+++ src/shared/functions.h
@@ -6,6 +6,9 @@
 #include <GL/glew.h> 
 #include <GLFW/glfw3.h>
 #include <stb_image.h>
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
 
 #include "helper/RootDir.h"
 #include "data.h"
@@ -139,4 +142,62 @@ GLuint createTexture(ImageData imageData, int unit)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     return texture;
+}
+
+void loadMeshFromFile(std::string fileName, std::vector<float>& vertices, std::vector<unsigned int>& indices)
+{
+    std::filesystem::path pathToFile(ROOT_DIR);
+    pathToFile = pathToFile / "res" / fileName;
+
+    Assimp::Importer importer;
+
+    // And have it read the given file with some example postprocessing
+    // Usually - if speed is not the most important aspect for you - you’ll
+    // probably to request more postprocessing than we do in this example.
+    const aiScene* scene = importer.ReadFile(pathToFile.string(),
+        aiProcess_CalcTangentSpace | aiProcess_Triangulate | aiProcess_JoinIdenticalVertices |
+        aiProcess_SortByPType | aiProcess_PreTransformVertices);
+
+    // If the import failed, report it
+    if (!scene) {
+        std::cerr << "Importing of 3D scene failed: " << importer.GetErrorString() << std::endl;
+        return;
+    }
+    if (!scene->HasMeshes()) return;
+    
+    aiMesh* mesh = scene->mMeshes[0]; // Just load the first mesh in this example
+    
+    for (unsigned int j = 0; j < mesh->mNumVertices; j++) {
+        // Copy the vertex data into the vector "vertices"
+        aiVector3D& v = mesh->mVertices[j];
+        vertices.push_back(v.x);
+        vertices.push_back(v.y);
+        vertices.push_back(v.z);
+
+        if (mesh->HasVertexColors(0))
+        {
+            // Use vertex colors if they are available
+            aiColor4D& c = mesh->mColors[0][j];
+            vertices.push_back(c.r);
+            vertices.push_back(c.g);
+            vertices.push_back(c.b);
+        }
+        else 
+        {
+            // In this case we use the position as the color attribute
+            vertices.push_back(v.x);
+            vertices.push_back(v.y);
+            vertices.push_back(v.z);
+        }
+    }
+    
+    for (unsigned int k = 0; k < mesh->mNumFaces; k++) {
+        aiFace& face = mesh->mFaces[k];
+        // Copy the index values to the vector "indices" above
+        // We can assume that there are only 3 indices per face
+        // because we set the aiProcess_Triangulate flag during the import.
+        for (unsigned int j = 0; j < face.mNumIndices; j++) {
+            indices.push_back(face.mIndices[j]);
+        }
+    }
 }
